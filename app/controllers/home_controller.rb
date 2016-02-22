fa4440c941c4d2c6635c954ab929f6766ecb96b4
Henry Jenkins <henry@henryjenkins.name>
2016-02-22 15:02:00 -0500

    Events on home page
    Side col of events with pagination.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
-  NEWS_ITEM_PAGINATION_LIMIT = 10
+  NEWS_ITEM_PAGINATION_LIMIT = 4
+  EVENT_PAGINATION_LIMIT = 4
 
   def index
-    @page = params[:page] || 0
-    @events = Event.published.limit(2)
-    @news_items = NewsItem.published.page(@page).per(NEWS_ITEM_PAGINATION_LIMIT)
+    @event_page = params[:news_item_page]
+    @news_item_page = params[:news_item_page]
+
+    @events = Event.published.page(@event_page).per(EVENT_PAGINATION_LIMIT)
+    @news_items = NewsItem.published.page(@news_item_page).per(NEWS_ITEM_PAGINATION_LIMIT)
   end
 end
